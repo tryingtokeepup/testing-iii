8984a71d048f9f94e615117540972920f4c3de72
tryingtokeepup <kl.hawaii@gmail.com>
2019-01-31 10:18:06 -0800

    wee, cleaned up MVP.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,6 +1,8 @@
 // Test away!
 import React from 'React';
 import { render, fireEvent } from 'react-testing-library';
+import 'jest-dom/extend-expect';
+import 'react-testing-library/cleanup-after-each';
 
 import Controls from './Controls';
 
@@ -12,5 +14,30 @@ describe('how the component <Controls /> should work', () => {
 
     fireEvent.click(lockBtn);
     expect(lockBtn.disabled).toBe(true);
+  });
+
+  it('closed toggle button should call the passed function', () => {
+    const mockFn = jest.fn(); //mocking
+    const { getByText } = render(
+      <Controls toggleClosed={mockFn} closed={false} />
+    );
+    const button = getByText(/close gate/i);
+
+    fireEvent.click(button);
+
+    expect(mockFn).toHaveBeenCalledTimes(1);
+  });
+
+  it('should disable locked toggle button if the gate is open', () => {
+    const mockFn = jest.fn();
+    const { getByText } = render(
+      <Controls toggleClosed={mockFn} closed={false} />
+    );
+    const button = getByText(/lock gate/i);
+    expect(button).toBeDisabled();
+
+    fireEvent.click(button);
+
+    expect(mockFn).not.toHaveBeenCalled();
   });
 });
