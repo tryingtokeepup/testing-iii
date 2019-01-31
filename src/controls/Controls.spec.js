0ffeeff50ac55fbfb401e30973b60d155d0b52d0
tryingtokeepup <kl.hawaii@gmail.com>
2019-01-31 09:07:41 -0800

    Got some tests to work. mostly frivolous, but I am learning how the library works.

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,16 @@
 // Test away!
+import React from 'React';
+import { render, fireEvent } from 'react-testing-library';
+
+import Controls from './Controls';
+
+describe('how the component <Controls /> should work', () => {
+  it('should have the lock button unoperative when gate is open', () => {
+    const { getByText } = render(<Controls />);
+
+    const lockBtn = getByText(/lock gate/i);
+
+    fireEvent.click(lockBtn);
+    expect(lockBtn.disabled).toBe(true);
+  });
+});
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,28 @@
 // Test away
+import React from 'React';
+import { render, fireEvent } from 'react-testing-library';
+
+import Dashboard from './Dashboard';
+
+describe('the component named <Dashboard />', () => {
+  it('has its initial state in default state', () => {
+    // thank you Yasirah for your tips!
+    const { getByText } = render(<Dashboard />);
+
+    const initialLockStatus = getByText('Unlocked');
+    const initialLockToggleBtn = getByText('Lock Gate');
+    const initialGateStatus = getByText('Open');
+    const initialGateToggleBtn = getByText('Close Gate');
+
+    expect(initialLockStatus.textContent).toBe('Unlocked');
+    expect(initialLockToggleBtn.textContent).toBe('Lock Gate');
+    expect(initialGateStatus.textContent).toBe('Open');
+    expect(initialGateToggleBtn.textContent).toBe('Close Gate');
+  });
+  it('changes its gate status when gateToggleBtn is clicked', () => {
+    const { getByText } = render(<Dashboard />);
+
+    const gateToggleBtn = getByText(/Close Gate/i);
+    fireEvent.click(gateToggleBtn);
+  });
+});
